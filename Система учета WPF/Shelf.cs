5410d5acd14dbdd1da446a839ee7f86c1d02b714
REzzKO <anadaminus@gmail.com>
2024-10-23 15:58:27 +1000

    Добавьте файлы проекта.

---
+++ Система учета WPF/Shelf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Система_учета_WPF
+{
+   public partial class Shelf
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// название &quot;стеллажа&quot;
+        /// </summary>
+        public string Title { get; set; } = null!;
+
+        public int IdDevil { get; set; }
+
+        public int YearBuy { get; set; }
+
+        /// <summary>
+        /// макс кол-во применений
+        /// </summary>
+        public int UseCount { get; set; }
+
+        /// <summary>
+        /// кол-во применений
+        /// </summary>
+        public int CurrentCount { get; set; }
+
+        public virtual Worker IdDevilNavigation { get; set; } = null!;
+    }
+}
---
+++ Система учета WPF/Worker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace Система_учета_WPF
+{
+    public partial class Worker
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// погоняло
+        /// </summary>
+        public string Nick { get; set; } = null!;
+
+        /// <summary>
+        /// кол-во душ
+        /// </summary>
+        public int Rank { get; set; }
+        public int Year { get; set; }
+
+        public virtual ICollection<Shelf> Racks { get; set; } = new List<Shelf>();
+    }
+}
